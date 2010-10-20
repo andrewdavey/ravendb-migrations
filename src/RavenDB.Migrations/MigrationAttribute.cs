a6649f6a05798a2628186d5e80417d44fff622ca
Andrew Davey <andrew@equin.co.uk>
2010-10-20 16:38:30 +0100

    Add basic src directory for code

---
+++ src/RavenDB.Migrations/MigrationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RavenDB.Migrations
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class MigrationAttribute : Attribute
+    {
+        public MigrationAttribute(int version)
+        {
+            Version = version;
+        }
+
+        public int Version { get; private set; }
+    }
+}
