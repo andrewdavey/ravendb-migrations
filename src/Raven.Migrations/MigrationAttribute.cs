b0e2226ceafe89220ba7b845437a623597ede5d1
Andrew Davey <andrew@equin.co.uk>
2010-10-26 16:37:08 +0100

    Enforce that migration attribute version is greater than zero.

--- src/Raven.Migrations/MigrationAttribute.cs
+++ src/Raven.Migrations/MigrationAttribute.cs
@@ -7,6 +7,7 @@ namespace Raven.Migrations
     {
         public MigrationAttribute(long version)
         {
+            if (version < 0) throw new ArgumentOutOfRangeException("version", "Migration version must be greater than zero.");
             Version = version;
         }
 
